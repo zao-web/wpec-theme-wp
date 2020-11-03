41be03031ef24d21dc93ede7e894bd66099aa01e
Justin Sainton <justinsainton@gmail.com>
2020-11-03 13:24:38 -0800

    Remove hard-coded reference in search action

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" id="search" method="get" class="search-form" action="http://wpec.local/">
+<form role="search" id="search" method="get" class="search-form" action="">
   <label for="search-field" class="screen-reader-text"><?php esc_html_e(' Search for something', 'simply' ); ?></label>
   <input type="search" id="search-field" class="search-field" placeholder="<?php esc_html_e(' Search for something ...', 'simply' ); ?>" value="" name="s">
   <input type="submit" class="search-submit" value="Search">
