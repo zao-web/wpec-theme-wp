8e9060140da2132f137ea4b4a15c4f9823473dd5
Liz Karaffa <liz@zao.is>
2020-11-03 16:57:45 -0800

    rename for more clarity

--- single.php
+++ single.php
@@ -25,7 +25,7 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'post' );
+				get_template_part( 'template-parts/content', 'single' );
 
 			endwhile; // End of the loop.
 			?>
