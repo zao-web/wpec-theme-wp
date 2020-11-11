dac57a9023eba43e5fe539f3ee2379d9e6e46db8
Liz Karaffa <liz@zao.is>
2020-11-10 16:57:47 -0800

    add caret button to menu list items with children for menu control

--- inc/hooks.php
+++ inc/hooks.php
@@ -116,3 +116,50 @@ function simply_shared_counts_location( $locations ) {
 	return $locations;
 }
 add_filter( 'shared_counts_theme_locations', 'simply_shared_counts_location' );
+
+/**
+ * Add a Sub Nav Toggle to the Primary Menu.
+ *
+ * @param stdClass $args An array of arguments.
+ * @param string   $item Menu item.
+ * @param int      $depth Depth of the current menu item.
+ *
+ * @return stdClass $args An object of wp_nav_menu() arguments.
+ */
+function simply_add_sub_toggles_to_main_menu( $args, $item, $depth ) {
+
+	// Add sub menu toggles to the Expanded Menu with toggles.
+	if ( isset( $args->show_toggles ) && $args->show_toggles ) {
+
+		// Wrap the menu item link contents in a div, used for positioning.
+		$args->before = '<div class="ancestor-wrapper">';
+		$args->after  = '';
+
+		// Add a toggle to items with children.
+		if ( in_array( 'menu-item-has-children', $item->classes, true ) ) {
+
+			$toggle_target_string = '.menu-modal .menu-item-' . $item->ID . ' > .sub-menu';
+			$toggle_duration      = simply_toggle_duration();
+
+			// Add the sub menu toggle.
+			$args->after .= '<button class="toggle sub-menu-toggle fill-children-current-color" data-toggle-target="' . $toggle_target_string . '" data-toggle-type="slidetoggle" data-toggle-duration="' . absint( $toggle_duration ) . '" aria-expanded="false"><span class="screen-reader-text">' . __( 'Show sub menu', 'simply' ) . '</span>' . simply_get_icon_svg( 'chevron-down' ) . '</button>';
+
+		}
+
+		// Close the wrapper.
+		$args->after .= '</div><!-- .ancestor-wrapper -->';
+
+		// Add sub menu icons to the primary menu without toggles.
+	} elseif ( 'primary' === $args->theme_location ) {
+		if ( in_array( 'menu-item-has-children', $item->classes, true ) ) {
+			$args->after = '<span class="icon"></span>';
+		} else {
+			$args->after = '';
+		}
+	}
+
+	return $args;
+
+}
+
+add_filter( 'nav_menu_item_args', 'simply_add_sub_toggles_to_main_menu', 10, 3 );
